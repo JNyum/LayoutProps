a19a330157cebaee9171774ba84e497233e68d36
JNyum <jjy142857@gmail.com>
2021-11-10 01:42:05 +0900

    Hw/ to. 교수님  button 연결은 다 했는데,,,

--- App.js
+++ App.js
@@ -1,50 +1,106 @@
-import React from "react";
-import {Button, ScrollView, View , StyleSheet, Text, Image, onpress } from "react-native";
+import React, { useState } from "react";
+import {Button, ScrollView, View , StyleSheet, Text, Image} from "react-native";
 import Constants from 'expo-constants';
 const App = () => {
-    const flexDirection = ['row', 'row-reverse', 'column', 'column-reverse'];
+    const flexDirections = ['row', 'row-reverse', 'column', 'column-reverse'];
+    const justifyContents = [
+        'flex-start',
+        'flex-end',
+        'center',
+        'space-between',
+        'space-around',
+        'space-evenly',
+    ];
+    const alignItems = ['stretch', 'flex-start', 'flex-end', 'center', 'baseline'];
+    const flexWraps = ['wrap', 'nowrap'];
+    const changeDirections = ['?']
+
+    const [flexDirectionIndex, setflexDirection] = useState(0);
+    const [justifyContentIndex, setJustifyContent] = useState(0);
+    const [alignItemsIndex, setalignItem] = useState(0);
+    const [flexWrapIndex, setflexWrap] = useState(0);
+    const [changeDirectionsIndex, setchangeDirect] = useState(0);
+    const hookedStyles = {
+        flexDirection:flexDirections[flexDirectionIndex],
+        justifyContent:justifyContents[justifyContentIndex],
+        alignItem:alignItems[alignItemsIndex],
+        flexWrap:flexWraps[flexWrapIndex],
+        changeDirection:changeDirections[changeDirectionsIndex],
+    }
+
+    const changeSetting = (value, options, setterfunction) => {
+        if (value == options.length - 1) {
+            setterfunction(0);
+        } else {
+            setterfunction(value + 1);
+        };
+        console.log(options[value]);
+    }
+
+    const Square = () => {
+        const sqStyle = {
+            width: 50,
+            height: 50,
+            backgroundColor: randomHexColor(),
+        };
+        return <View style={sqStyle} />
+    };
+
+    const[squares, setSquares] = useState([Square(),Square(),Square()]);
+
+    
     return (
         <>
             <View style={{paddingTop: Constants.statusBarHeight}}/>
-            <View style={[styles.container, styles.playingSpace]}>
-            
+            <View style={[styles.container, styles.playingSpace, hookedStyles]}>
+                {squares.map(elem => elem)}
                 
-
-            </View>
+                </View>
             <ScrollView style={[styles.container]}>
-         <View style={[styles.buttonView]}>
-                        <Button title="CHANGE FLEX DIRECTION"
-                            onPress={() =>
-                                console.log("press CHANGE FLEX DIRECTION")
-                            }
-                        />
-                    </View>
+                <View style={[styles.controlSpace]}>
                     <View style={[styles.buttonView]}>
-                        <Button title="CHANGE JUSTIFY CONTENT"
-                        onPress={() =>
-                            console.log("press CHANGE JUSTIFY CONTENT")
+                        <Button title="CHANGE FLEX DIRECTION"
+                            onPress={() => {
+                                console.log("press CHANGE FLEX DIRECTION");
+                                changeSetting(flexDirectionIndex, flexDirections, setflexDirection);
+                            }
                         }
                         />
                     </View>
                     <View style={[styles.buttonView]}>
+                        <Button title="CHANGE JUSTIFY CONTENT"
+                            onPress={() => {
+                                console.log("press CHANGE JUSTIFY CONTENT");
+                                changeSetting(justifyContentIndex, justifyContents, setJustifyContent);
+                                }
+                            }
+                        />
+                    </View>
+                    <View style={[styles.buttonView]}>
                         <Button title="CHANGE ALIGN ITEMS"
-                        onPress={() =>
+                        onPress={() => {
                             console.log("press CHANGE ALIGN ITEMS")
+                            changeSetting(alignItemsIndex, alignItems, setalignItem);
+                            }
                         }
                         />
                     </View>
                     <View style={[styles.buttonView]}>
                         <Button title="CHANGE DIRECTION"
-                        onPress={() =>
+                        onPress={() => {
                             console.log("press CHANGE DIRECTION")
+                            changeSetting(changeDirectionsIndex, changeDirections, setchangeDirect);    
                         }
+                    }
                         />
                     </View>
                     <View style={[styles.buttonView]}>
                         <Button title="CHANGE FLEX WRAP"
-                        onPress={() =>
+                        onPress={() => {
                             console.log("press CHANGE FLEX WRAP")
+                            changeSetting(flexWrapIndex, flexWraps, setflexWrap);
                         }
+                    }
                         />
                     </View>
                     <View style={[styles.buttonView]}>
@@ -62,7 +118,7 @@ const App = () => {
                         />
                     </View>
 
-             <View/>
+             </View>
                 
 
              </ScrollView>
@@ -86,6 +142,22 @@ const styles = StyleSheet.create({
     buttonView: {
         width:'50%',
         padding: 10,
-    }
+    },
+    tinylogo: {
+        width: 100,
+        height: 100,
+    },
+    
+
+    
 })
+
+const randomHexColor = () => {
+    return '#000000'.replace(/0/g, () => {
+        return (~~(Math.random() * 16)).toString(16);
+    });
+}
+
+
+
 export default App;
