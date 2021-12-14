54066d04b24add47649344d6eabece8650750a02
JNyum <jjy142857@gmail.com>
2021-12-14 18:39:51 +0900

    add weather screen

--- screens/Weather.js
+++ screens/Weather.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import {Text, Alert, StyleSheet, View} from 'react-native';
+import {Text, Alert, StyleSheet, View, Image} from 'react-native';
 import * as Location from 'expo-location';
 import Axios from 'axios';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 const apikey = process.env.REACT_APP_WEATHER_KEY;
 
+const weatherOptions = {
+    Clear: {
+        iconName: "weather-sunny"
+    },
+    Clouds: {
+        iconName: "weather-cloudy"
+    },
+    Rain: {
+        iconName: "weather-rain"
+    }
+}
+
+
 export default class Weather extends React.Component {
+
+    state = {
+        cond : "Clear"
+
+    }
 
     getLocation = async () => {
         try {
@@ -28,6 +47,11 @@ export default class Weather extends React.Component {
 
         );
         console.log(data);
+        console.log(data.main.temp);
+        console.log(data.main.temp_max);
+        console.log(data.main.temp_min);
+        console.log(data.weather[0].main);
+        this.setState({cond:data.weather[0].main, temp:data.main.temp, icon:data.weather[0].icon})
     }
 
     componentDidMount() {
@@ -35,10 +59,34 @@ export default class Weather extends React.Component {
     }
 
     render() {
+        const {cond, temp, icon} = this.state;
         return (
             <>
+            <View style={styles.container}>
+                <View style={styles.halfContainer}>
+                <MaterialCommunityIcons name={weatherOptions[cond].iconName} size={24} color="black" />
+                <Text style={styles.temptitile}>temp : {temp}℃</Text>
+                </View>
+            </View>
             </>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        justifyContent: "center",
+        alignItems : "center"
+    },
+    halfContainer : {
+        flex : 1,
+        justifyContent : "center",
+        alignItems : "center"
+    },
+    temptitile : {
+        fontSize : 24
+    }
+
+
+})
